a312c8e08e9149d42105be894b134eda06671010
hp <helder@yarilabs.com>
2022-10-10 21:45:11 +0100

    wip setup with passwords usernames

--- redis-sentinel/sentinel-entrypoint.sh
+++ redis-sentinel/sentinel-entrypoint.sh
@@ -3,5 +3,9 @@
 sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /redis/sentinel.conf
+
+sed -i "s/\$REDIS_MASTER_PASSWORD/$REDIS_MASTER_PASSWORD/g" /redis/sentinel.conf
+sed -i "s/\$REDIS_SENTINEL_USERNAME/$REDIS_SENTINEL_USERNAME/g" /redis/sentinel.conf
+sed -i "s/\$REDIS_SENTINEL_PASSWORD/$REDIS_SENTINEL_PASSWORD/g" /redis/sentinel.conf
  
 redis-server /redis/sentinel.conf --sentinel
